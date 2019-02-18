14245477b2937521f68fc2d068a7f350b13704f2
cleptes <jan.subelj010@gmail.com>
2019-02-18 21:45:14 +0100

    počistu .scripts

--- sp_init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo pacman -S nodejs npm mongodb mongodb-tools
-
-# express install
-# https://expressjs.com/en/starter/installing.html
-
--- sync_on_shutdown.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo test > ~/test
-rsync -avu --delete /home/cleptes/Programming/ /home/cleptes/HDD_Documents/programming/
