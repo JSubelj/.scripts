9f164d556ca2b07aa7cc4452cf0588cd2eea549d
cleptes <jan.subelj010@gmail.com>
2019-02-18 21:20:55 +0100

    before everything goes to shit 2

--- toggle-sound-sink.sh
+++ toggle-sound-sink.sh
@@ -1,42 +1,21 @@
 #!/bin/bash 
-function string_parse {
-	sink="$(python << END
-import sys
 
-sinks="""$sinks"""
-sinks=sinks.split("\tname:")[1:]
-sinks=[y.replace(" ","").replace("<","").replace(">","").replace("\n","") for y in sinks]
-
-default_sink="""$def_sink"""
-default_sink=default_sink.split("Default Sink: ")[1]
-
-index_of_def_sink = sinks.index(default_sink)
-
-if index_of_def_sink+1 == len(sinks):
-        print(0)
-else:
-        print(index_of_def_sink+1)
-END
-)"
-echo $sink
-}
+sinks=`pacmd list-sinks | grep name: | grep -o "alsa_output[^>]*"`
+current=`pactl info | grep 'Default Sink:' | grep -o "alsa_output.*"`
+for s in $sinks;
+do
+	if [[ "$s" != "$current" ]]; then next="$s"; break; fi
+done
 
 
 
-sinks=`pacmd list-sinks | grep name:`
-def_sink=`pactl info | grep 'Default Sink:'`
-string_parse 
-
-
-
-
-echo "Setting default sink to: $sink";
-pacmd set-default-sink $sink
+echo "Setting default sink to: $next";
+pacmd set-default-sink $next
 pacmd list-sink-inputs | grep index | while read line
 do
 echo "Moving input: ";
 echo $line | cut -f2 -d' ';
-echo "to sink: $sink";
-pacmd move-sink-input `echo $line | cut -f2 -d' '` $sink
+echo "to sink: $next";
+pacmd move-sink-input `echo $line | cut -f2 -d' '` $next
 
 done
